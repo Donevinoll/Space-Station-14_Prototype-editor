8714d38926031ca53166942c6f9f128d0f1e237a
VlaDOS1408 <vlad.buklovsky@gmail.com>
2024-05-17 13:34:47 +0400

    Добавьте файлы проекта.

---
+++ Space-Station-14_Prototype-editor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Space_Station_14_Prototype_editor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
